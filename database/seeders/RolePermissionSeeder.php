ba1386df6ef99018c79addd9304f75276062492d
ian.andilimawan <ian.andilimawan@redtech.co.id>
2026-08-08 21:16:22 +0700

    chore: update admin creation log info in seeder

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -18,15 +18,15 @@ class RolePermissionSeeder extends Seeder
 
         // Create Admin User
         $admin = User::firstOrCreate(
-            ['email' => 'admin@redtech.co.id'],
+            ['email' => 'hi.intechstudio@gmail.com'],
             [
                 'name' => 'Administrator',
-                'password' => Hash::make('redtech.co.id'),
+                'password' => Hash::make('intechstudio.id'),
                 'email_verified_at' => now(),
             ]
         );
 
-        $this->command->info('Admin user created: admin@retech.co.id / redtech.co.id');
+        $this->command->info('Admin user created: hi.intechstudio@gmail.com / intechstudio.id');
 
 
 
